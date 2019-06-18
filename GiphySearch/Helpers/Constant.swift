3a00d731f3440545c84d43e9da431e001d71e90c
invision <info@invisionsolutions.ca>
2019-06-18 20:54:44 +0500

    project setup and design in progress

---
+++ GiphySearch/Helpers/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  GiphySearch
+//
+//  Created by Invision-MacBookPro-shayan on 18/06/2019.
+//  Copyright © 2019 Invision-MacBookPro-F011. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+	static let apiKey = "JMLSmv9u9XHDOjERu7mPhIXVUlAlnJzv"
+}
+
+struct CellIdentifiers {
+	static let giphyCell = "giphyCell"
+}
---
+++ GiphySearch/Views/GiphyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GiphyCollectionViewCell.swift
+//  GiphySearch
+//
+//  Created by Invision-MacBookPro-shayan on 18/06/2019.
+//  Copyright © 2019 Invision-MacBookPro-F011. All rights reserved.
+//
+
+import UIKit
+
+class GiphyCollectionViewCell: UICollectionViewCell {
+    
+}
